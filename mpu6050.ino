6eeec577b4fdecc02ad3876a87e7be46292c0180
BenMor99 <moradkhanibehnam@gmail.com>
2021-10-22 11:29:46 +0330

    Arduino Code Updated

--- mpu6050.ino
+++ mpu6050.ino
@@ -10,49 +10,29 @@ MPU6050 mpu;
 
 #define OUTPUT_READABLE_QUATERNION
 
-// #define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_YAWPITCHROLL
 
-#define INTERRUPT_PIN 2
-#define LED_PIN 13
-bool blinkState = false;
+bool dmpReady = false;
+uint8_t devStatus;
+uint8_t fifoBuffer[64];
 
-bool dmpReady = false;  
-uint8_t mpuIntStatus;   
-uint8_t devStatus;      
-uint16_t packetSize;    
-uint16_t fifoCount;     
-uint8_t fifoBuffer[64]; 
-
-Quaternion q;           
-VectorInt16 aa;         
-VectorInt16 aaReal;    
-VectorInt16 aaWorld;    
-VectorFloat gravity;    
-float euler[3];         
-float ypr[3];           
-
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
-volatile bool mpuInterrupt = false;
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
+Quaternion q;
+VectorFloat gravity;
+float ypr[3];
 
 void setup() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
-        Wire.setClock(400000); 
+        Wire.setClock(400000);
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
 
-    
     Serial.begin(115200);
     while (!Serial);
 
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
 
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
@@ -72,24 +52,13 @@ void setup() {
         Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
-        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-        Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
-
-        packetSize = mpu.dmpGetFIFOPacketSize();
     } 
     else {
         Serial.print(F("DMP Initialization failed (code "));
         Serial.print(devStatus);
         Serial.println(F(")"));
     }
-
-    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -107,18 +76,14 @@ void loop() {
         #endif
 
         #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
             mpu.dmpGetQuaternion(&q, fifoBuffer);
             mpu.dmpGetGravity(&gravity, &q);
             mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print(ypr[2] * 180/M_PI);
+            Serial.print(ypr[2] * 180/M_PI); // roll
             Serial.print(",");
-            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print(ypr[1] * 180/M_PI); // pitch
             Serial.print(",");
-            Serial.println(ypr[0] * 180/M_PI);
+            Serial.println(ypr[0] * 180/M_PI); // yaw
         #endif
-
-        blinkState = !blinkState;
-        digitalWrite(LED_PIN, blinkState);
     }
 }
